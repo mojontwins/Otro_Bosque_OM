86194f3bca4aae42078b2502286ed8efa354e3d7
na_th_an <nathan.asshantti@gmail.com>
2020-03-17 12:11:23 +0100

    First Commit

---
+++ lib/update-libs.bat
@@ -0,0 +1,8 @@
+@echo off
+copy cpcrslib\cpcrslib.lib c:\z88dk10-backup-orig\lib\clibs\
+copy cpcrslib\cpcrslib.h c:\z88dk10-backup-orig\include\
+copy cpcwyzlib\cpcwyzlib.lib c:\z88dk10-backup-orig\lib\clibs\
+copy cpcwyzlib\cpcwyzlib.h c:\z88dk10-backup-orig\include\
+copy splib2\splib2.lib c:\z88dk10-backup-orig\lib\clibs\
+copy splib2\spritepack.h c:\z88dk10-backup-orig\include\
+echo Updated!
